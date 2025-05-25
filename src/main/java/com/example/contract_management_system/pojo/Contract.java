f55a8e6a132dd242cd4428ec9049d6bda1ca78ce
AXI <3306778393@qq.com>
2025-05-25 19:31:00 +0800

    合同起草

--- src/main/java/com/example/contract_management_system/pojo/Contract.java
+++ src/main/java/com/example/contract_management_system/pojo/Contract.java
@@ -3,6 +3,7 @@ package com.example.contract_management_system.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
 import java.util.Date;
@@ -12,10 +13,11 @@ import java.util.Date;
 public class Contract {
     @TableId(value = "num", type = IdType.AUTO) // 主键，自动递增
     private Integer num;             // 合同编号
-
     private String name;             // 合同名称
     private Integer customer;        // 客户编号
+    @TableField("begin_time")
     private Date beginTime;          // 开始时间
+    @TableField("end_time")
     private Date endTime;            // 结束时间
     private String content;          // 合同内容
     private Integer userId;          // 起草人ID（外键关联users表）
