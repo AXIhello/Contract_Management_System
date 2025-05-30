001438bea002ea14b17fbf351c6bec556db9a073
cat-cat-web <changbai_2023@163.com>
2025-05-30 11:28:35 +0800

    待定稿

--- src/main/java/com/example/contract_management_system/controller/ContractController.java
+++ src/main/java/com/example/contract_management_system/controller/ContractController.java
@@ -1,8 +1,12 @@
 package com.example.contract_management_system.controller;
 
+import com.example.contract_management_system.dto.ContractPendingDTO;
 import com.example.contract_management_system.pojo.Contract;
 import com.example.contract_management_system.service.ContractService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/contracts")
@@ -17,6 +21,12 @@ public class ContractController {
     @PostMapping
     public boolean createContract(@RequestBody Contract contract) {
         return contractService.createContract(contract);
+    }
+
+    @GetMapping("/getToBeFinishedContracts")
+    public List<ContractPendingDTO> getToBeFinishedContracts() {
+        System.out.println("✔ 已进入 getToBeFinishedContracts 控制器");
+        return contractService.getToBeFinishedContracts();
     }
 
 } 
